e4c955b208f859e5bc14f83b6f163a726559a421
Artur <lzatos.off@gmail.com>
2025-11-18 18:54:45 +0500

    Cоздана библиотека классов и классы FileInfo и ViewModelsend/message

---
+++ Server/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+namespace Server
+{
+    public class Program
+    {
+        public static List<User> Users = new List<User>();
+        public static IPAddress IpAdress;
+        public static int Port;
+        static void Main(string[] args)
+        {
+         
+        }
+
+        public static bool AutorizationUser(string login, string password)
+        {
+            User user = null;
+            user = Users.Find(x=> x.login == login && x.password == password);
+            return user != null;
+        }
+
+        public static List<string> GetDirectory(string src)
+        {
+            List<string> FoldersFiles = new List<string>();
+            if(Directory.Exists(src))
+            {
+                string[] dirs = Directory.GetDirectories(src);
+                foreach(string dir in dirs)
+                {
+                    string NameDirectory = dir.Replace(src, "");
+                    FoldersFiles.Add(NameDirectory + "/");
+                }
+
+                string[] files = Directory.GetFiles(src);
+                foreach(string file in files)
+                {
+                    string NameFile = file.Replace(src, "");
+                    FoldersFiles.Add(NameFile);
+                }
+            }
+            return FoldersFiles;
+        }
+
+
+        public static void StartServer()
+        {
+            IPEndPoint endPoint = new IPEndPoint(IpAdress, Port);
+            Socket sListener = new Socket(
+                AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            sListener.Bind(endPoint);
+            sListener.Listen(10);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Сервер запущен.");
+            while(true)
+            {
+                try
+                {
+                    Socket Handler = sListener.Accept();
+                    string Data = null;
+                    byte[] Bytes = new byte[10485760];
+                }
+            }
+        }
+    }
+}
