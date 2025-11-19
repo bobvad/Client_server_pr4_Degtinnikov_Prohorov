39b14647eacb2c367c0de6d785bf8cd41f4861b7
qwelord <secondofall22@rambler.ru>
2025-11-19 15:14:10 +0500

    Прописанные переменные.Дегтянников.

---
+++ Client/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using Common;
+using Newtonsoft.Json;
+
+namespace Client
+{
+    public class Program
+    {
+
+        public static IPAddress IpAdress;
+        public static int Port;
+        public static int Id = -1;
+        
+    }
+}
