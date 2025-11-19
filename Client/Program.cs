bcb3f6730f450b09227773b8e8e3e229251134fe
qwelord <secondofall22@rambler.ru>
2025-11-19 15:15:22 +0500

    Прописанный метод CheckCommand.Дегтянников.

--- Client/Program.cs
+++ Client/Program.cs
@@ -13,5 +13,54 @@ namespace Client
         public static int Port;
         public static int Id = -1;
         
+        public static bool CheckCommand(string message)
+        {
+            bool BCommand = false;
+            string[] DataMessage = message.Split(new string[1] { " " }, StringSplitOptions.None);
+
+            if (DataMessage.Length > 0 )
+            {
+               string Command = DataMessage[0];
+                if (Command == "connect")
+                {
+                    if(DataMessage.Length != 3)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: connect [login] [password]\nПример: connect User1 P@ssw0rd");
+                        BCommand = false;
+                    }
+                    else
+                     BCommand = true;
+                }
+                else if(Command == "cd")
+                    BCommand = true;
+
+                else if(Command == "get")
+                {
+                    if (DataMessage.Length == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red;
+                        Console.WriteLine("Использование: get [NameFile]\nПример: get Test.txt");
+                        BCommand = false;
+                    }
+                    else
+                        BCommand= true;
+                }
+                else if(Command == "set")
+                {
+                    if(DataMessage.Length == 1)
+                    {
+                        Console.ForegroundColor = ConsoleColor.Red; 
+                        Console.WriteLine("Использование: set [NameFile]\nПример: set Test.txt");
+                        BCommand = false;
+                    }
+                    else
+                        BCommand = true;
+                }
+            }
+
+            return BCommand;
+        }
+   
     }
 }
